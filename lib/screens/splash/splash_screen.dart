c02c6224900d164d4f994cb4c1fffaabf998dd70
ggarma <gonzalo.garma@sportscam.pe>
2023-10-19 11:43:45 -0500

    logo updated

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:spedtracker_app/components/background/background.dart';
 import 'package:spedtracker_app/screens/login/login_screen.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -25,16 +27,30 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SizedBox(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        child: const Center(
-          child: Text(
-            "SPENDTRACKER",
-            style: TextStyle(fontSize: 32),
+      body: Stack(alignment: Alignment.bottomCenter, children: [
+        const Background(),
+        SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Center(
+            child: Container(
+              height: 200,
+              width: 200,
+              padding: const EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                  borderRadius: const BorderRadius.all(Radius.circular(20)),
+                  color: SchedulerBinding
+                              .instance.platformDispatcher.platformBrightness ==
+                          Brightness.light
+                      ? const ColorScheme.light().background
+                      : const Color.fromRGBO(116, 107, 85, 1)),
+              child: Image.asset(
+                "assets/logo.jpeg",
+              ),
+            ),
           ),
         ),
-      ),
+      ]),
     );
   }
 }
