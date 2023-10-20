cd32c465f5c1950eb6a47f3b2666dccdc4f1280c
JesusGraos <jesusgraos1419@gmail.com>
2023-10-19 21:28:24 -0500

    Abstract alert implementation

--- lib/components/alerts/products/abstract_alert.dart
+++ lib/components/alerts/products/abstract_alert.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
-/*1
+
 abstract class AbstractAlert {
   Future showAlertDialog(
       BuildContext context, String title, String body, Function accept);
 }
-1*/
+
